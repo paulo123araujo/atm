fe20fff7e23f8b053c6b3be2c1a2865df7937cb6
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2021-12-31 00:53:01 -0300

    write tests to method to withdraw and show user balance

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,15 @@
+import { ATM } from '@src/atm';
+import { User } from '@src/user';
+
 describe('success case', () => {
   test('should create a cash machine with a user', () => {
-    const initialBalance = 0;
+    const initialBalance = 120;
     const user = new User(initialBalance);
-    const atm = new ATM();
+    const atm = new ATM(user);
+
+    const withdraw = 30;
+    atm.withdraw(withdraw);
+
+    expect(atm.showUserBalance()).toEqual((90).toFixed(2));
   });
 });
