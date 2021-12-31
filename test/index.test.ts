d6b332003b879f74b5850e2537bba656d2023422
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2021-12-31 00:34:35 -0300

    initial test

---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+describe('success case', () => {
+  test('should create a cash machine with a user', () => {
+    const initialBalance = 0;
+    const user = new User(initialBalance);
+    const atm = new ATM();
+  });
+});
